7854b969d7af1b57d36ac038e462be1e8c91fc98
Edgar Mancia <eddiemm@users.noreply.github.com>
2021-12-14 16:08:47 -0800

    edit comments

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,16 +1,22 @@
-console.log("ready");
-
 const accordionTriggers = document.getElementsByClassName('accordion__trigger');
 
+/* handle trigger click */
 const accordionMousedown = (evt) => {
     const trigger = evt.target;
+
+    /* toggle active class */
     trigger.classList.toggle('active');
 
+    /* target div after the trigger */
     const content = trigger.nextElementSibling;
+
+    /* change the height property for transition to take effect */
     content.style.height ? content.style.height = null: content.style.height = content.scrollHeight + 'px'; 
 }
 
+
 let i;
+/* add a listener to each trigger */
 for(i = 0; i < accordionTriggers.length; i++){
     accordionTriggers[i].addEventListener('mousedown', accordionMousedown);
 }
